093f0e2c0be58c075842cf8dc3757a2441846f50
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-02-16 11:23:18 -0800

    Implemented coins service

--- src/controllers/coins.controller.ts
+++ src/controllers/coins.controller.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
+import CoinsService from '../services/coins.service';
 
 class CoinsController {
   public getCoins = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     // TODO: use res.query.??? to get url query params, ??? is name of the query param
     try {
       // TODO: add service call
-      res.status(200).json({});
+      const total = req.query.total as string
+      const service = new CoinsService();
+      const coins = await service.getCoins(total);
+      res.status(200).json(coins);
     } catch (error) {
       next(error);
     }
--- src/services/coins.service.ts
+++ src/services/coins.service.ts
@@ -4,6 +4,23 @@ class CoinsService {
   public getCoins = async (totalNum: string): Promise<CoinsDto> => {
     const coinsDto = new CoinsDto();
     // TODO: Add service logic
+    const [dollars, cents] = totalNum.split('.').map((n) => +n);
+
+    coinsDto.total = +totalNum;
+    coinsDto.coins = {
+      bars: {
+        1: ~~(+totalNum % 2),
+        2: ~~(dollars / 2),
+      },
+      foos: {
+        1: +(1 === cents),
+        5: +(5 === cents),
+        10: +(10 === cents),
+        20: +(20 === cents),
+        50: +(50 === cents),
+      }
+    };
+
     return coinsDto;
   };
 }
